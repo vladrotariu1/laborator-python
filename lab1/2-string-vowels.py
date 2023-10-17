ab24e7e4dfdd9208a174b9f72124d068903e4d93
vrotariu1 <vladrotariu24@gmail.com>
2023-10-17 16:47:27 +0300

    problems corrected

--- lab1/2-string-vowels.py
+++ lab1/2-string-vowels.py
@@ -1,6 +1,6 @@
 from functools import reduce
 
 string = str(input('Enter string: '))
-numb_vowels = reduce(lambda cumulator, current_char: cumulator + 1 if current_char in 'aeiou' else cumulator, string, 0)
+numb_vowels = reduce(lambda cumulator, current_char: cumulator + 1 if current_char.lower() in 'aeiou' else cumulator, string, 0)
 
 print(numb_vowels)
--- lab1/3-numb-occurrences-substring.py
+++ lab1/3-numb-occurrences-substring.py
@@ -1,6 +1,10 @@
 substring = str(input('Enter first string: '))
 string = str(input('Enter second string: '))
 
-numb_occurrences = string.count(substring)
+numb_occurrences = 0
+
+for i in range(len(string) - len(substring) + 1):
+    if string[i:i + len(substring)] == substring:
+        numb_occurrences += 1
 
 print(numb_occurrences)
